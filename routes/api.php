3b96f165ce52f0638df7dd4c44087f1f7a4117f6
deep <mohameddepo677@gmail.com>
2024-08-09 23:29:45 +0300

    add other file for testing branch

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |help me
+|help me two
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
